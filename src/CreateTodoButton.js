7a54c5fb801bf82611a85f709f20c53840c44560
migmar46 <muneramiguel7@gmail.com>
2021-12-23 11:56:33 -0500

    hhh

---
+++ src/CreateTodoButton.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './CreateButton.css';
+
+function CreateTodoButton(props) {
+  return (
+    <button className="CreateTodoButton">+</button>
+  );
+}
+
+export { CreateTodoButton };
